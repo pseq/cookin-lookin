ca7930e333d7b8d92c0fe7ec2e63164f945349cd
Pseq <tataka.post@gmail.com>
2022-06-16 22:34:21 +0300

    Initial Commit

---
+++ cookin lookin/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  cookin lookin
+//
+//  Created by pseq on 16.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
