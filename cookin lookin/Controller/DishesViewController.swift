3ddee251c33854acbd3d8b10687c15ec07904cc5
Pseq <tataka.post@gmail.com>
2022-06-17 13:50:19 +0300

    Add TableView stuff

--- cookin lookin/Controller/DishesViewController.swift
+++ cookin lookin/Controller/DishesViewController.swift
@@ -10,11 +10,27 @@ import CoreData
 
 class DishesViewController: UITableViewController {
 
+    var dishesArr = [Dishes]()
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    //MARK: TableView stuff -
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return dishesArr.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "DishCell", for: indexPath)
+        let dish = dishesArr[indexPath.item]
+        
+        // todo заменить deprecated textLabel 
+        cell.textLabel?.text = dish.name
+        
+        return cell
+    }
 }
 
